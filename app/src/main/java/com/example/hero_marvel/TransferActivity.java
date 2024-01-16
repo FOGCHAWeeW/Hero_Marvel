47a48540123d23413480e43b930c92246a418fdd
PoTaTo <danimir.iv@gmail.com>
2024-01-16 20:59:47 +0300

    add new feature

--- app/src/main/java/com/example/hero_marvel/TransferActivity.java
+++ app/src/main/java/com/example/hero_marvel/TransferActivity.java
@@ -2,13 +2,32 @@ package com.example.hero_marvel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class TransferActivity extends AppCompatActivity {
+    private TextView power;
+    private TextView zahita;
+    private TextView weapone;
+    private TextView magik;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transfer);
+
+
+    }
+
+    public void go(View view) {
+        Intent intent = new Intent(this, FightActivity.class);
+        startActivity(intent);
+    }
+
+    public void inventory(View view) {
+        Intent intent = new Intent(this, InventoryActivity.class);
+        startActivity(intent);
     }
 }
